278b4dd00e6ba2ad4c7f5d42822538add6ed91b3
Noura Jazbeh <dr.noorj19@gmail.com>
2024-01-21 19:55:11 +0100

    todo React

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Todolist from './Todolist';
+
+function App() {
+  return (
+  
+    <div className="App">
+      <header className="App-header">
+        <h1>Meiine TODO-List</h1>
+        <Todolist/>
+      </header>
+    </div>    
+  );
+}   
+ 
+
+export default Todolist;
